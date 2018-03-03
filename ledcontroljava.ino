d02c2aa5daa0d02d240fb15e7b6236484ce730a5
Wai Phyo Aung <eye03.rd@gmail.com>
2018-03-03 09:28:47 -0800

    Add files via upload

---
+++ ledcontroljava.ino
@@ -0,0 +1,22 @@
+int led = 13;
+void setup() {
+pinMode(led,OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() {
+ String str = Serial.readString();
+ if(str.length() >0)
+ {
+  if(str == "ON")
+  {
+    digitalWrite(led,HIGH);
+  }
+  else if(str == "OFF")
+  {
+    digitalWrite(led,LOW);
+  }
+ }
+
+}
